dc19089339484c8cfc21f7da26f80bdcfd2fc341
adarshk24 <adarshkajrekar3@gmail.com>
2021-07-18 09:26:12 -0700

    regester and login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask.helpers import url_for
+from app.forms import LoginForm, RegistrationForm
+from flask import render_template , redirect , flash
 from app import app
 
 
@@ -6,3 +8,16 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created ')
+        return redirect(url_for('login'))
+    return render_template('registration.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    return render_template('login.html',form=form)
